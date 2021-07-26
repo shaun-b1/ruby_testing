caee1cca7537bbd0af93906370ed18109011b563
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2021-07-26 18:57:35 +1000

    Finished lesson

---
+++ lib/calculator.rb
@@ -0,0 +1,9 @@
+class Calculator
+  def add(*args)
+    args.sum
+  end
+
+  def subtract(a, b)
+    a - b 
+  end
+end
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,22 @@
+require './lib/calculator'
+
+describe Calculator do
+  describe "#add" do
+    it "returns the sum of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2)).to eql(7)
+    end
+
+    it "returns the sum of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(2, 5, 7)).to eql(14)
+    end
+  end
+
+  describe "#subtract" do
+    it "Takes the first term and removes the second, returning the difference" do
+      calculator = Calculator.new
+      expect(calculator.subtract(5, 2)).to eql (3)
+    end
+  end 
+end
